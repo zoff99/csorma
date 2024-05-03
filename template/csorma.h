27025f77a11d387bc962772b8ceb028996e5b057
zoff99 <zoff99@users.noreply.github.com>
2024-05-03 20:28:33 +0200

    turn off TRACE

--- template/csorma.h
+++ template/csorma.h
@@ -25,7 +25,7 @@ static const char csorma_global_version_asan_string[] = "0.99.0-ASAN";
 // ----------- version -----------
 // ----------- version -----------
 
-#define CSORMA_TRACE 1 // to log all SQL statements set this to 1
+#define CSORMA_TRACE 0 // to log all SQL statements set this to 1
 
 #define __SELECT_START "SELECT * FROM "
 #define __SELECT_COUNT_START "SELECT count(*) as count FROM "
