d27b6a8ce82109f6860a311b7839f4a2f0ce3447
zoff99 <zoff99@users.noreply.github.com>
2024-05-19 21:18:38 +0200

    add call to OrmaDatabase_key and add some comments

--- template/_csorma_stub1.c
+++ template/_csorma_stub1.c
@@ -15,3 +15,15 @@ int main()
     const char *db_dir = "./";
     const char *db_filename = "stub.db";
     OrmaDatabase *o = OrmaDatabase_init((uint8_t*)db_dir, strlen(db_dir), (uint8_t*)db_filename, strlen(db_filename));
+
+#ifdef ENCRYPT_CSORMA
+    // HINT: if `ENCRYPT_CSORMA` is defined then csorma was compiled with `sqlcipher`
+    // in case you need to do different things on encrypted databases, you can test for this define.
+    printf("STUB: csorma was compiled with sqlcipher encryption\n");
+#endif
+
+    // HINT: you can call `OrmaDatabase_key` even when sqlcipher is not used.
+    //       then it will return `CSORMA_GENERIC_RESULT_ERROR` and normal unencrypted sqlite is used.
+    const char *orma_secret_key = "secret007$%";
+    int key_result = OrmaDatabase_key(o, (uint8_t*)orma_secret_key, strlen(orma_secret_key));
+    printf("STUB: setting sqlcipher key. result = %d\n", key_result);
