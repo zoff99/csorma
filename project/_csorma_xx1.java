ff0767da34819bb15930c3889f82a5fe773f3a9e
zoff99 <zoff99@users.noreply.github.com>
2024-11-16 14:30:36 +0100

    add new table template

---
+++ project/_csorma_xx1.java
@@ -0,0 +1,17 @@
+
+@Table
+public class xx1
+{
+    @PrimaryKey(autoincrement = true, auto = true)
+    public long pid;
+
+    @Column
+    public String txt1;
+
+    @Column
+    public int int1;
+
+    @Column
+    public boolean bool1;
+
+
